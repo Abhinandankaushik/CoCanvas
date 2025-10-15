8d88f6e6c1fc553ed3412b7b06fb4fd22861bb3a
abhinandan kaushik <abhinandankaushik2004@gmail.com>
2025-10-16 01:56:59 +0530

    added zodSchema/common shared JWT token

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,7 +1,48 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "@repo/backend-common/config";
+import { middleware } from "./middleware";
+import { createUserSchema, SinginSchema, CreateRoomSchema } from "@repo/common/types"
 
 const app = express()
 
-app.listen(3001,()=>console.log("express backend"))
 
-app.get("/",(req,res)=>res.send("hello from express backend"))
+
+app.post("/signup", (req, res) => {
+
+    const data = createUserSchema.safeParse(req.body)
+
+    if (!data.success) {
+        return res.json({
+            message: "Incorrect inputs"
+        })
+    }
+
+    //db call
+    res.json({
+        userId: 123
+    })
+})
+
+
+app.post("/signin", (req, res) => {
+
+    const userId = 1;
+    const token = jwt.sign({ userId }, JWT_SECRET);
+
+    res.json({
+        token
+    })
+})
+
+
+app.post("/room", middleware, (req, res) => {
+
+    //db call
+    res.json({
+        roomId: 123
+    })
+})
+
+
+app.listen(3001, () => console.log("express backend runnig"))
--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,10 +1,29 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken"
+import { JWT_SECRET } from "@repo/backend-common/config"
+import { } from "@repo/common/types"
+const wss = new WebSocketServer({ port: 5000 })
 
-const wss = new WebSocketServer({port : 5000})
+wss.on('connection', (socket, request) => {
 
-wss.on('connection',(socket)=>{
-    console.log("user connected")
-    socket.on("message",(data)=>{
-            socket.send("pong")
+    const url = request.url;
+    if (!url) {
+        return;
+    }
+
+    const queryParams = new URLSearchParams(url.split('?')[1]);
+    const token = queryParams.get('token') ?? ""
+    const decoded = jwt.verify(token, JWT_SECRET)
+
+
+    if (!decoded || !(decoded as JwtPayload).userId) {
+        socket.close();
+        return;
+    }
+
+
+    socket.on('message', function message(data) {
+        socket.send('pong')
     })
+
 })
